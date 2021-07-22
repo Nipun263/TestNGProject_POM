22ac2ad5324d9ab7d9e767e226d4779789f27f60
Verma <nipun.verma@fisglobal.com>
2021-07-22 19:28:38 +0530

    Final Commit

--- src/test/java/Utility/ScreenShots.java
+++ src/test/java/Utility/ScreenShots.java
@@ -1,5 +1,21 @@
 package Utility;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 public class ScreenShots {
+	
+	public static WebDriver driver ;
+	
+	public void captureScreenShot() throws IOException {
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(file,new File(""));
+	}
 
 }
